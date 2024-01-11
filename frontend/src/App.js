b0299d0c63e41575c9b0d91a5ec48aa6271d1cf8
ysfj32ni <ysfjaadouni@gmail.com>
2024-01-11 12:11:16 +0100

    front NOT Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import StudentList from './components/StudentList';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div className=' border border-white rounded-lg w-[50%] h-[50%]'>
-        {/* <button
-          className="font-bold  hover:opacity-[100%] bg-[#d2d7db]"
-          onClick={() => {
-            
-          }}
-          >
-          Login with Google
-        </button> */}
-        {/* <StudentList/> */}
+        <SearchBar/>
+        <StudentList/>
         </div>
       </header>
     </div>
---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,13 @@
+const SearchBar = () => {
+    
+    return (    
+        <div>
+            <form>
+                <input type="text" placeholder="Search..." className="" ></input>      
+            </form>
+        </div>
+    );
+
+}
+
+export default SearchBar;
